345ebf847a8381ada5205127b65748cd81e7973f
Chris Wolf <chriswolf@chriss-mbp.home>
2017-06-22 10:54:55 -0400

    "pset0: change all double quotes to single quotes to fit standard Python styling"

--- pset0/ps0.py
+++ pset0/ps0.py
@@ -4,12 +4,12 @@
 # Time: 0.05
 
 # Take in user's birthday
-print "Enter your date of birth"
-birthday = raw_input("")
+print 'Enter your date of birth'
+birthday = raw_input('')
 
 # Take in user's last name
-print "Enter your last name"
-name = raw_input("")
+print 'Enter your last name'
+name = raw_input('')
 
 # print this information back out
 print name, birthday
