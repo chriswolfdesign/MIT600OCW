9484ab5b9909acd380ab1f623c0c6089e45fee90
Chris Wolf <chriswolf@chriss-mbp.home>
2017-06-22 16:17:28 -0400

    "Completed (success): Problem Set 1"

--- pset1/ps1b.py
+++ pset1/ps1b.py
@@ -1,20 +1,15 @@
+# Problem Set 1
+# Name: Chris Wolf
+# E-mail: chriswolfdesign@gmail.com
+# Time: 0:30
+
 MONTHS = 12
 
 original_balance = float(raw_input \
-    ("Enter the outstanding balance on your credit card: "))
+    ('Enter the outstanding balance on your credit card: '))
 yearly_interest = float(raw_input \
-    ("Enter the annual credit card interest rate as a decimal: "))
+    ('Enter the annual credit card interest rate as a decimal: '))
 monthly_interest = yearly_interest / MONTHS
-
-"""
-Debug
-
-print "Balance:", original_balance
-print "Yearly interest:", yearly_interest
-print "Monthly interest:", monthly_interest
-
-End Debug
-"""
 
 balance = original_balance
 payment = 0
@@ -25,10 +20,10 @@ while balance >= 0:
     while month <= MONTHS:
         balance = round(balance * (1 + monthly_interest) - payment, 2)
         if balance <= 0:
-            print "RESULT"
-            print "Monthly payment to pay off debt in one year", payment
-            print "Number of months needed:", month
-            print "Balance:", balance
+            print 'RESULT'
+            print 'Monthly payment to pay off debt in one year', payment
+            print 'Number of months needed:', month
+            print 'Balance:', balance
             break
         else:
             month += 1
---
+++ pset1/ps1c.py
@@ -0,0 +1,43 @@
+# Problem Set 1
+# Name: Chris Wolf
+# E-mail: chriswolfdesign@gmail.com
+# Time: 0:30
+
+MONTHS = 12
+EPSILON = -0.12
+
+original_balance = float(raw_input \
+    ('Enter the oustanding balance on your credit card: '))
+yearly_interest = float(raw_input \
+    ('Enter the annual credit card interest rate as a decimal: '))
+monthly_interest = yearly_interest / MONTHS
+
+# The amount paid equally between months assuming no interest
+# (less than the amount we could possibly be charged)
+lower_bound = round(original_balance / 12, 2)
+
+# The amount piad equally between months assuming there is interest
+# (more than the amount we could possibly be charged)
+upper_bound = round((original_balance * \
+    (1 + (yearly_interest / MONTHS))**MONTHS) / MONTHS, 2)
+
+balance = original_balance
+payment = round((upper_bound + lower_bound) / 2, 2)
+
+while balance > 0 or balance < EPSILON:
+    balance = original_balance
+
+    for month in range(MONTHS):
+        balance = round(balance * (1 + monthly_interest) - payment, 2)
+
+    if balance > 0:
+        lower_bound = payment
+        payment = round((upper_bound + lower_bound) / 2, 2)
+    elif balance < EPSILON:
+        upper_bound = payment
+        payment = round((upper_bound + lower_bound) / 2, 2)
+
+print 'RESULT'
+print 'Monthly payment to pay off debt in 1 year:', payment
+print 'Number of months needed: 12'
+print 'Balance:', balance
