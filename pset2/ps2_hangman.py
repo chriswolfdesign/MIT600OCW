6493f160687979aef417a99ab9e75797bb792388
Chris Wolf <chriswolf@Chriss-MBP.Home>
2017-06-23 18:59:15 -0400

    "Completed (success): pset2

--- pset2/ps2_hangman.py
+++ pset2/ps2_hangman.py
@@ -1,8 +1,9 @@
 # 6.00 Problem Set 3
-# 
+#
 # Hangman
 #
 
+GUESSES_MAX = 8
 
 # -----------------------------------
 # Helper code
@@ -15,7 +16,7 @@ WORDLIST_FILENAME = "words.txt"
 def load_words():
     """
     Returns a list of valid words. Words are strings of lowercase letters.
-    
+
     Depending on the size of the word list, this function may
     take a while to finish.
     """
@@ -40,9 +41,71 @@ def choose_word(wordlist):
 # end of helper code
 # -----------------------------------
 
-# actually load the dictionary of words and point to it with 
+def concealed_current_word(answer, found):
+    """
+    answer (string): the correct answer for the game
+    found (dict string -> Boolean): a dictionary representing whether or not
+        a specific letter in our word had been guessed yet
+
+    Returns a string of the answer with unguessed characters "hidden"
+    """
+
+    cache = ''
+    for letter in answer:
+        if found[letter]:
+            cache += letter
+        else:
+            cache += '_'
+        cache += ' '
+    return cache
+
+# actually load the dictionary of words and point to it with
 # the wordlist variable so that it can be accessed from anywhere
 # in the program
 wordlist = load_words()
+answer = choose_word(wordlist)
+guesses_left = GUESSES_MAX
 
-# your code begins here!
+found = {}
+for letter in answer:
+    found[letter] = False
+
+available_letters = 'abcdefghijklmnopqrstuvwxyz'
+
+print 'Welcome to the game, Hangman!'
+print 'I am thinking of a word that is', len(answer), 'letters long.'
+print '------------'
+
+while guesses_left > 0:
+    print 'You have', guesses_left, 'guesses left'
+    print 'Available letters:', available_letters
+
+    current_guess = raw_input('Please guess a letter: ')
+    current_guess = current_guess.lower()
+    available_letters = available_letters.replace(current_guess, '')
+
+    if current_guess in found:
+        found[current_guess] = True
+        print 'Good guess:', concealed_current_word(answer, found)
+    else:
+        guesses_left -= 1
+        print 'Oops!  That letter is not in my word:', \
+            concealed_current_word(answer, found)
+
+    print '-----------'
+
+    # See if the player has won the game and react accordingly
+    game_won = True
+    for letter in found:
+        if not found[letter]:
+            game_won = False
+
+    if game_won:
+        print 'Congrulations, you won!'
+        break
+
+    # See if the player has lost the game and react accordingly
+    # BTW, I just lost The Game typing that last comment :_(
+    if guesses_left < 1:
+        print 'I\'m sorry, you are out of usable guesses'
+        print 'Correct answer:', answer
