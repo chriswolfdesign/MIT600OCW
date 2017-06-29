b3b91b5248b25c93f3e82fb9d249ff26d422b964
Chris Wolf <chriswolf@Chriss-MBP.Home>
2017-06-29 08:13:32 -0400

    "Attempted (success): Problem 1 pset 3"

--- pset3/ps3a.py
+++ pset3/ps3a.py
@@ -11,6 +11,7 @@ import string
 VOWELS = 'aeiou'
 CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
 HAND_SIZE = 7
+FULL_HAND_BONUS = 50
 
 SCRABBLE_LETTER_VALUES = {
     'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
@@ -76,7 +77,13 @@ def get_word_score(word, n):
     word: string (lowercase letters)
     returns: int >= 0
     """
-    # TO DO...
+    score = 0
+    for letter in word:
+        score += SCRABBLE_LETTER_VALUES[letter]
+    score *= len(word)
+    if len(word) == n:
+        score += FULL_HAND_BONUS
+    return score
 
 #
 # Make sure you understand how this function works and what it does!
